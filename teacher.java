6aef2669617bb71bd5186d6774af227c90c93659
threegold <601956422@qq.com>
2022-05-04 19:26:29 +0800

    commit 2

--- teacher.java
+++ teacher.java
@@ -1,5 +1,6 @@
 import java.util.*;
 class teacher{
-   String name;  //老师姓名
-   String tno;   //老师工号
+   private String name;  //老师姓名
+   private String tno;   //老师工号
+   private string email;   //修改的内容
 }
