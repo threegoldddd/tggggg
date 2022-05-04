682c700a49eaca567840d8171e17cce6b49be3ee
threegold <601956422@qq.com>
2022-05-04 19:36:22 +0800

    commit four

--- student.java
+++ student.java
@@ -1,5 +1,7 @@
 import java.util.*
 class student{
-  private string name;
-  private string address;
+      private string name;
+      private string address;
+      private string email;
+      private String telephone;   //新增电话属性
 }
